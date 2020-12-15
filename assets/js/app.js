c4869ccb78e9fa2bbc70eb48c2004548b44713a2
Nathan Opresnik <nopresnik@me.com>
2020-12-15 16:21:45 +1100

    Move local storage functions into separate file

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,38 +11,6 @@ $("#search-form").on("submit", function (e) {
   }
 });
 
-function getSearchHistory() {
-  return JSON.parse(localStorage.getItem("search-history"));
-}
-
-function saveSearch(search) {
-  var search = search.toLowerCase().trim();
-  var history = [];
-  if (getSearchHistory() !== null) {
-    history = getSearchHistory();
-  }
-
-  if (!history.includes(search)) {
-    if (history.length >= 8) history.pop();
-    history.unshift(search);
-    localStorage.setItem("search-history", JSON.stringify(history));
-
-    renderSearchHistory();
-  }
-}
-
-function getSelectedCity() {
-  var city = localStorage.getItem("selected-city");
-
-  // Return the selected city or default to Melbourne if none.
-  return city ? city : "Melbourne";
-}
-
-function setSelectedCity(city) {
-  localStorage.setItem("selected-city", city);
-  renderSelectedCity();
-}
-
 function init() {
   renderSearchHistory();
   renderSelectedCity();
