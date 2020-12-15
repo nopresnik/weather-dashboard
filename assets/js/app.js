f284b7bfc46c5701dd5a80f5af3eb6ca27278926
Nathan Opresnik <nopresnik@me.com>
2020-12-15 16:18:30 +1100

    Move rendering functions into separate file

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,31 +11,6 @@ $("#search-form").on("submit", function (e) {
   }
 });
 
-function renderSearchHistory() {
-  var searchMenu = $(".nav-menu");
-  searchMenu.empty();
-
-  var listItem = function (name) {
-    var item = $('<a href="#">');
-    item.append($("<li>").text(name));
-    item.data("name", name);
-    item.on("click", function (e) {
-      e.preventDefault();
-      setSelectedCity($(this).data("name"));
-    });
-
-    return item;
-  };
-
-  getSearchHistory().forEach((item) => {
-    searchMenu.append(listItem(item));
-  });
-}
-
-function renderSelectedCity() {
-  $("#sel-city-name").text(getSelectedCity());
-}
-
 function getSearchHistory() {
   return JSON.parse(localStorage.getItem("search-history"));
 }
---
+++ assets/js/rendering.js
@@ -0,0 +1,24 @@
+function renderSearchHistory() {
+  var searchMenu = $(".nav-menu");
+  searchMenu.empty();
+
+  var listItem = function (name) {
+    var item = $('<a href="#">');
+    item.append($("<li>").text(name));
+    item.data("name", name);
+    item.on("click", function (e) {
+      e.preventDefault();
+      setSelectedCity($(this).data("name"));
+    });
+
+    return item;
+  };
+
+  getSearchHistory().forEach((item) => {
+    searchMenu.append(listItem(item));
+  });
+}
+
+function renderSelectedCity() {
+  $("#sel-city-name").text(getSelectedCity());
+}
