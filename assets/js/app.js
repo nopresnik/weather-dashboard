eeb6089138f4639ef45594ede1b5b7b93e874691
Nathan Opresnik <nopresnik@me.com>
2020-12-15 16:16:19 +1100

    Remove jquery ready function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,77 +1,76 @@
-$(document).ready(function () {
-  $("#search-form").on("submit", function (e) {
-    e.preventDefault();
+// Open Weather API Key
+var apiKey = "d1aed9e0dce49c617b21383eb6b512c6";
 
-    if ($("#search-input").val()) {
-      saveSearch($("#search-input").val());
+$("#search-form").on("submit", function (e) {
+  e.preventDefault();
 
-      $("#search-input").val("");
-    }
-  });
+  if ($("#search-input").val()) {
+    saveSearch($("#search-input").val());
 
-  function renderSearchHistory() {
-    var searchMenu = $(".nav-menu");
-    searchMenu.empty();
-
-    var listItem = function (name) {
-      var item = $('<a href="#">');
-      item.append($("<li>").text(name));
-      item.data("name", name);
-      item.on("click", function (e) {
-        e.preventDefault();
-        setSelectedCity($(this).data("name"));
-      });
-
-      return item;
-    };
-
-    getSearchHistory().forEach((item) => {
-      searchMenu.append(listItem(item));
-    });
+    $("#search-input").val("");
   }
-
-  function renderSelectedCity() {
-    $("#sel-city-name").text(getSelectedCity());
-  }
-
-  function getSearchHistory() {
-    return JSON.parse(localStorage.getItem("search-history"));
-  }
-
-  function saveSearch(search) {
-    var search = search.toLowerCase().trim();
-    var history = [];
-    if (getSearchHistory() !== null) {
-      history = getSearchHistory();
-    }
-
-    if (!history.includes(search)) {
-      if (history.length >= 8) history.pop();
-      history.unshift(search);
-      localStorage.setItem("search-history", JSON.stringify(history));
-
-      renderSearchHistory();
-    }
-
-    console.log(history);
-  }
-
-  function getSelectedCity() {
-    var city = localStorage.getItem("selected-city");
-
-    // Return the selected city or default to Melbourne if none.
-    return city ? city : "Melbourne";
-  }
-
-  function setSelectedCity(city) {
-    localStorage.setItem("selected-city", city);
-    renderSelectedCity();
-  }
-
-  function init() {
-    renderSearchHistory();
-    renderSelectedCity();
-  }
-
-  init();
 });
+
+function renderSearchHistory() {
+  var searchMenu = $(".nav-menu");
+  searchMenu.empty();
+
+  var listItem = function (name) {
+    var item = $('<a href="#">');
+    item.append($("<li>").text(name));
+    item.data("name", name);
+    item.on("click", function (e) {
+      e.preventDefault();
+      setSelectedCity($(this).data("name"));
+    });
+
+    return item;
+  };
+
+  getSearchHistory().forEach((item) => {
+    searchMenu.append(listItem(item));
+  });
+}
+
+function renderSelectedCity() {
+  $("#sel-city-name").text(getSelectedCity());
+}
+
+function getSearchHistory() {
+  return JSON.parse(localStorage.getItem("search-history"));
+}
+
+function saveSearch(search) {
+  var search = search.toLowerCase().trim();
+  var history = [];
+  if (getSearchHistory() !== null) {
+    history = getSearchHistory();
+  }
+
+  if (!history.includes(search)) {
+    if (history.length >= 8) history.pop();
+    history.unshift(search);
+    localStorage.setItem("search-history", JSON.stringify(history));
+
+    renderSearchHistory();
+  }
+}
+
+function getSelectedCity() {
+  var city = localStorage.getItem("selected-city");
+
+  // Return the selected city or default to Melbourne if none.
+  return city ? city : "Melbourne";
+}
+
+function setSelectedCity(city) {
+  localStorage.setItem("selected-city", city);
+  renderSelectedCity();
+}
+
+function init() {
+  renderSearchHistory();
+  renderSelectedCity();
+}
+
+init();
