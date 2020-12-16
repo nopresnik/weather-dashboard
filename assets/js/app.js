6f48e41e654c5e693ab2b2dac74d3cd32ba27eb3
Nathan Opresnik <nopresnik@me.com>
2020-12-16 21:04:20 +1100

    Move api key to new remotedata.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,3 @@
-// Open Weather API Key
-var apiKey = "d1aed9e0dce49c617b21383eb6b512c6";
-
 $("#search-form").on("submit", function (e) {
   e.preventDefault();
 
---
+++ assets/js/remotedata.js
@@ -0,0 +1,2 @@
+// Open Weather API Key
+var apiKey = "d1aed9e0dce49c617b21383eb6b512c6";
