0c43e0b94ac65a9723c25e0bc80e81a64c805833
Nathan Opresnik <nopresnik@me.com>
2020-12-18 18:07:07 +1100

    Forecast API call

--- assets/js/remotedata.js
+++ assets/js/remotedata.js
@@ -1,2 +1,18 @@
 // Open Weather API Key
 var apiKey = "d1aed9e0dce49c617b21383eb6b512c6";
+var apiForecastURL = "http://api.openweathermap.org/data/2.5/forecast";
+
+// api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+function getForecast(city) {
+  // console.log(`${apiForecastURL}?q=${city}&appid=${apiForecastURL}`);
+  $.ajax({
+    url: `${apiForecastURL}?q=${city}&units=metric&appid=${apiKey}`,
+    success: function (res) {
+      parseForecast(res);
+    },
+  });
+}
+
+function parseForecast(data) {
+  console.log(data);
+}
